0f0b0c553f4f7c9d3ca73a3fba7620286a8a6222
Rafael Martins <ramartins.redes@outlook.com>
2023-07-28 15:04:28 -0300

    Commit inicial agora vai

---
+++ src/main/java/jezzeramartins/jezzrawebapp/JezzrawebappApplication.java
@@ -0,0 +1,13 @@
+package jezzeramartins.jezzrawebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JezzrawebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JezzrawebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jezzeramartins/jezzrawebapp/JezzrawebappApplicationTests.java
@@ -0,0 +1,13 @@
+package jezzeramartins.jezzrawebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JezzrawebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
